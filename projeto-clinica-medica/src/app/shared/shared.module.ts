0c67a55e2136ac48dc269ad9f0f71d367054a35a
Wagner Paulino <wagnersantos9778@gmail.com>
2018-09-23 14:18:34 -0300

    Projeto Front-end

---
+++ projeto-clinica-medica/src/app/shared/shared.module.ts
@@ -0,0 +1,54 @@
+import { DashboardComponent } from './../dashboard/dashboard.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatSnackBarModule,
+  MatSelectModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatIconModule,
+  MatSidenavModule,
+  MatListModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatCheckboxModule,
+  MatDialogModule} from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+const ANGULAR_MATERIAL = [
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatListModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatSnackBarModule,
+    MatCheckboxModule,
+    MatDialogModule
+];
+
+const ANGULAR = [
+  CommonModule,
+  HttpClientModule,
+  RouterModule,
+  FormsModule
+];
+
+@NgModule({
+  imports: [
+    ANGULAR_MATERIAL,
+    ANGULAR
+  ],
+  declarations: [
+    DashboardComponent
+  ],
+  exports: [
+    ANGULAR_MATERIAL,
+    ANGULAR,
+    DashboardComponent
+  ]
+})
+export class SharedModule { }
